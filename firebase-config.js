ec57f88b22de1a50dd9cebe679be52a676139be6
nawabzadahacker0-beep <nawabzadahacker0@gmail.com>
2026-09-21 03:50:18 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,14 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB25VaErJEsI3VLBeb52cpczKRmEWC4fEs",
-  authDomain: "pak-earning-site.firebaseapp.com",
-  databaseURL: "https://pak-earning-site-default-rtdb.firebaseio.com",
-  projectId: "pak-earning-site",
-  storageBucket: "pak-earning-site.firebasestorage.app",
-  messagingSenderId: "830671389706",
-  appId: "1:830671389706:web:16ab555ffdd85cff70cbf3",
-  measurementId: "G-0CBTF67JM6"
+    apiKey: "AIzaSyB25VaErJEsI3VLBeb52cpczKRmEWC4fEs",
+    authDomain: "pak-earning-site.firebaseapp.com",
+    databaseURL: "https://pak-earning-site-default-rtdb.firebaseio.com",
+    // projectId: "pak-earning-site",
+    storageBucket: "pak-earning-site.firebasestorage.app",
+    messagingSenderId: "830671389706",
+    appId: "1:830671389706:web:16ab555ffdd85cff70cbf3",
+    measurementId: "G-0CBTF67JM6"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-window.auth = firebase.auth();
-window.db  = firebase.firestore();
-window.storage = firebase.storage();
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
